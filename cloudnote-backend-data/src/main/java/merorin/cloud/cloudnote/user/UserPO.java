80159660ed8b699186246d91481cc5a9c5941832
merorin <343932227@qq.com>
2017-10-24 16:46:07 +0800

    删除无用文件,添加一个类

---
+++ cloudnote-backend-data/src/main/java/merorin/cloud/cloudnote/user/UserPO.java
@@ -0,0 +1,11 @@
+package merorin.cloud.cloudnote.user;
+
+/**
+ * Description: write down the usage of class here...
+ *
+ * @author guobin On date 2017/10/24.
+ * @version 1.0
+ * @since jdk 1.8
+ */
+public class UserPO {
+}
